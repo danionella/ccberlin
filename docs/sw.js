538728dd5cb4859de42fde6994b2f16b0bdfab60
janamach <janamach.bio@gmail.com>
2023-04-03 22:31:00 +0200

    update site

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230403.222901';
+const cacheName = 'chirpy-20230403.223100';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
