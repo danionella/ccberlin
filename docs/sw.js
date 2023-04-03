7a6d5efbb81b781a1add508931353cd5726434eb
janamach <janamach.bio@gmail.com>
2023-04-03 22:34:53 +0200

    update site

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230403.223100';
+const cacheName = 'chirpy-20230403.223453';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
--- update.sh
+++ update.sh
@@ -3,7 +3,10 @@ cd chirpy-starter-main/
 bundle exec jekyll b
 cd ../
 
+rm -rf docs/*
 rsync -vr chirpy-starter-main/_site/ docs/
+
+echo "circuitsclub.com" > docs/CNAME
 
 git add .
 git commit -m "update site"
